1acb5468f975c0aa56b0fcc61e6d7fb97a4d8025
DorisML <dml2410@gmail.com>
2022-02-13 19:48:13 -0500

    Se modificÃaron las validaciones en PublicMemoryDto

--- src/main/java/co/edu/practice/tse/dtos/PublicMemoryDto.java
+++ src/main/java/co/edu/practice/tse/dtos/PublicMemoryDto.java
@@ -8,6 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,12 +18,14 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class PublicMemoryDto extends PrivateMemoryDto {
+    @NotNull
     private List<VisualizationDto> visualizationList;
-    private int numViews;
+    @Null
+    private Integer numViews;
 
-    public PublicMemoryDto(String id, String name, LocalDate memoryDate, LocalDate creationDate, EnumVisibilityDto visibility, List<String> tagList, String creatorId, List<MemoryPhotoDto> memoryPhotoList, LocationDto location, List<VisualizationDto> visualizationList, int numViews) {
+    public PublicMemoryDto(String id, String name, LocalDate memoryDate, LocalDate creationDate, EnumVisibilityDto visibility, List<String> tagList, String creatorId, List<MemoryPhotoDto> memoryPhotoList, LocationDto location, List<VisualizationDto> visualizationList) {
         super(id, name, memoryDate, creationDate, visibility, tagList, creatorId, memoryPhotoList, location);
         this.visualizationList = visualizationList;
-        this.numViews = numViews;
+        this.numViews = 0;
     }
 }
